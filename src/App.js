51fd7ae08d43073bf11b7fbc05d899778c5d5f02
Alex' Le Coq <alexandre.lcoq@gmail.com>
2019-10-10 11:23:38 +0200

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Contact from './components/Contact.js';
+import './components/Contact.css';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Contact 
+       name='Arnold Horton' 
+       avatar='https://randomuser.me/api/portraits/men/82.jpg'
+       online
+      />
+      <Contact 
+       name='Krin Burke' 
+       avatar='https://randomuser.me/api/portraits/women/84.jpg'
+      />
+      <Contact 
+       name='Landon Wells' 
+       avatar='https://randomuser.me/api/portraits/men/0.jpg'
+       online
+      />
     </div>
   );
 }
---
+++ src/components/Contact.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Contact.css';
+
+const online = {
+    IsOnline : true,
+    name: 'online',
+}
+
+function Contact(props) {
+    return (
+        <div className='Contact'>
+            
+            <img className='avatar' 
+                src={props.avatar} 
+                alt={props.name} />
+
+            <p className='name'>{props.name}</p>
+            
+            <p className='status'>
+                <span className={props.online ? 'status-online' : 'status-offline'}></span>
+                <span className='status-text'>{props.online ? 'online' : 'offline'}</span>
+            </p>
+        </div>
+    )
+}
+
+export default Contact;
